125e7768b860879aab4b1c54399b74a554eb9806
wangzong <130238481+wangzong123123@users.noreply.github.com>
2023-11-11 18:42:49 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oEoq46C1z-2NSOqggRxwAjVSXRLY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'z9O-0vx9rnt6Cxzzg3F8A6cslvzDnXc67XqU7YHV254',
+      useTemplateId: 'CbNmvlsI_8G4h_8EbnVJtJP-IEUlt6xQ0wRGNO88wwc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
